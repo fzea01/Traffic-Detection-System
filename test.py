ac258490a5e83e1019532f6ae503612da4012e16
fzea <nuttahpon_best@hotmail.com>
2018-05-08 01:13:46 +0700

    initial commit

---
+++ test.py
@@ -0,0 +1,54 @@
+#import libraries of python opencv
+import cv2
+import numpy as np
+
+#create VideoCapture object and read from video file
+cap = cv2.VideoCapture('./videos/k2.MP4')
+
+
+#use trained cars XML classifiers
+car_cascade = cv2.CascadeClassifier('./data/cascade1.xml')
+#bicycle_cascade = cv2.CascadeClassifier('./data/bicycle.xml')
+#bus_cascade = cv2.CascadeClassifier('./data/bus.xml')
+#motorbike_cascade = cv2.CascadeClassifier('./data/motorbike.xml')
+
+#read until video is completed
+while True:
+    #capture frame by frame
+    ret, frame = cap.read()
+
+    #convert video into gray scale of each frames
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #detect cars in the video
+    cars = car_cascade.detectMultiScale(gray, 1.1, 3)
+    #bicycle = bicycle_cascade.detectMultiScale(gray, 1.1, 3)
+    #bus = bus_cascade.detectMultiScale(gray, 1.1, 3)
+   # motorbike = motorbike_cascade.detectMultiScale(gray, 1.1, 3)
+    
+
+
+    #to draw arectangle in each cars 
+    for (x,y,w,h) in cars:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+
+    #for (x,y,w,h) in bicycle:
+    #    cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+      
+    #for (x,y,w,h) in bus:
+    #    cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+      
+    #for (x,y,w,h) in motorbike:
+    #   cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+      
+      
+
+    #display the resulting frame
+    cv2.imshow('video', frame)
+    #press Q on keyboard to exit
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+#release the videocapture object
+cap.release()
+#close all the frames
+cv2.destroyAllWindows()
